fb0fc02fa24102635720ba8b4985c11b55eab1b4
jiangzhiheng <1689991551@qq.com>
2020-02-06 11:51:26 +0800

    ..

---
+++ Shell/Shell_Scripts/安装docker/dockerInstall.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#This Scripts is used by install docker-ce 
+# Pre install
+cd /etc/yum.repos.d/
+mkdir /etc/yum.repos.d/bak
+mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/bak
+curl http://mirrors.163.com/.help/CentOS7-Base-163.repo > base.repo
+yum install -y wget 
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Install Docker-CE
+cd /etc/yum.repos.d/
+wget https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo &>/dev/null
+yum -y install docker-ce
+# Docker-ce Version
+# yum list docker-ce.x86_64 --showduplicates | sort -r
+# 参考文档
+# https://developer.aliyun.com/mirror/docker-ce?spm=a2c6h.13651102.0.0.3e221b11nOtc6w
+# 配置镜像加速
+mkdir /etc/docker
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://pync0a1m.mirror.aliyuncs.com"]
+}
+EOF
+
+# Tips
+cat > /etc/sysctl.conf <<-'EOF'
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl -p
+
+systemctl start docker.service
+systemctl enable docker.service
